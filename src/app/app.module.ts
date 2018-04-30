17548be063f81e3491023f1eb7b296be78d47318
Thiago Bressanin <thiago.bressanin@flytour.com.br>
2018-04-30 12:57:56 -0300

    feat(routing): Added RoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
-
+import { MapModule } from './map/map.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MapComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
