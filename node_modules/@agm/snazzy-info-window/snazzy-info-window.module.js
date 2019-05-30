4dbe07d4346d35b22f0af063a47f7e95e1581d70
Thiago Bressanin <thiago.bressanin@iterative.com.br>
2019-05-30 14:39:54 -0300

    a

---
+++ node_modules/@agm/snazzy-info-window/snazzy-info-window.module.js
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { AgmSnazzyInfoWindow } from './directives/snazzy-info-window';
+var AgmSnazzyInfoWindowModule = /** @class */ (function () {
+    function AgmSnazzyInfoWindowModule() {
+    }
+    AgmSnazzyInfoWindowModule.decorators = [
+        { type: NgModule, args: [{
+                    declarations: [AgmSnazzyInfoWindow],
+                    exports: [AgmSnazzyInfoWindow]
+                },] },
+    ];
+    return AgmSnazzyInfoWindowModule;
+}());
+export { AgmSnazzyInfoWindowModule };
+//# sourceMappingURL=snazzy-info-window.module.js.map
---
+++ node_modules/@agm/snazzy-info-window/snazzy-info-window.module.ngfactory.d.ts
@@ -0,0 +1,3 @@
+import * as i0 from '@angular/core';
+import * as i1 from './snazzy-info-window.module';
+export declare const AgmSnazzyInfoWindowModuleNgFactory: i0.NgModuleFactory<i1.AgmSnazzyInfoWindowModule>;
